96a6150112177dc0ebc6bfdfa4c09c31ff207fc2
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2024-12-02 12:54:58 +0530

    fix:ui_0.1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar :React.FC = () => {
                             {session?.image ? <img src={session?.image} alt="profile" className="rounded-full" width={50} height={40} /> : 
                             <div className="bg-gray-800 border-dashed text-white border-purple-700 border-  [1px]   md:border-[2px] rounded-full h-10 w-10 md:h-14 md:w-14 flex flex-row items-center justify-center">{session.email? session.email.charAt(0).toUpperCase(): ''}</div>}
                             <Link href='/dashboard' className="px-6 justify-center items-center flex">
-                                <div className=" px-4 py-2  items-center justify-center flex flex-row text-purple-700 font-medium text-xl lg:text-2xl 2k:text-3xl rounded-lg border border-purple-700 hover:text-white hover:bg-purple-700 dark:hover:bg-slate-900 ">
+                                <div className=" px-4 py-2  items-center justify-center flex flex-row text-purple-700 font-medium text-xl lg:text-2xl 2k:text-3xl rounded-lg border border-purple-700  hover:text-white hover:bg-purple-700 dark:hover:bg-slate-900 ">
                                     Dashboard
                                 </div>
                             </Link>
@@ -47,7 +47,7 @@ const Navbar :React.FC = () => {
                 <form action ={logout} className="items-center justify-center flex flex-col gap-4">
                         {session.image ? <img src={session?.image} alt="profile" className="rounded-full " width={40} height={40} /> : <div className="bg-gray-800 border-dashed text-white border-purple-700 border-[1px] md:border-[2px] rounded-full h-10 w-10 md:h-14 md:w-14 flex flex-row items-center justify-center">{session.email? session.email.charAt(0).toUpperCase(): ''}</div>}
                         <Link href='/dashboard' className="px-2 justify-center items-center flex">
-                                <div className=" px-4 py-2  items-center justify-center flex flex-row text-white font-medium text-xl rounded-lg border border-purple-700 hover:bg-purple-400 dark:hover:bg-slate-900 ">
+                                <div className=" px-4 py-2  items-center justify-center flex flex-row text-purple-700 dark:text-white font-medium text-xl rounded-lg border border-purple-700 hover:bg-purple-400 dark:hover:bg-slate-900 ">
                                     Dashboard
                                 </div>
                             </Link>
